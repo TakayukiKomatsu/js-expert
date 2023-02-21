f3158fd4154f5cb07061db284cd3438c613466b0
Takayuki Komatsu <komatsutakayuki235@live.com>
2023-02-21 11:59:12 -0300

    feat: Add aula 01 module-4

---
+++ src/4-modulo/demo01-this-apply-call-arguments/index.js
@@ -0,0 +1,24 @@
+const { watch, promises: { readFile } } = require('fs')
+
+
+class File {
+    watch(event, filename) {
+        console.log('this', this)
+        console.log('arguments', Array.prototype.slice.call(arguments))
+        this.showContent(filename)
+    }
+
+    async showContent(filename) {
+        console.log((await readFile(filename)).toString())
+    }
+}
+
+const file = new File()
+
+file.watch.call({
+    showContent: () => console.log('call: hey sinon')
+}, null, __filename)
+
+file.watch.apply({
+    showContent: () => console.log('call: hey sinon')
+}, [null, __filename])
